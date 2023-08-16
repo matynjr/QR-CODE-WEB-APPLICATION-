fcef79b65ad481502226654712f1e425b5b5ac53
matynjr <akua@AKUAs-MacBook-Pro.local>
2023-08-16 10:47:57 +0300

    started adding routes

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,6 +1,7 @@
 //for handling header links
 //create header links here
 //add more links
+//add more links
 const Header =()=>{
     return(
         <div><a href="#"><button>Generate QR Codes</button></a></div>
