9dce8c05762340e4eaea2b23fac39db0a602c3e0
ncarol <nakaweesicarol7@gmail.cim>
2023-08-20 15:44:27 +0300

    Login And SignUp Page

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,4 +1,108 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Signup() {
-  return <div className="page-title"> SIGNGUP PAGE</div>;
+  const navigateTo = useNavigate();
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');  
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const handleFirstnameChange = (event) => {
+    setFirstname(event.target.value);
+  };
+  const handleLastnameChange = (event) => {
+    setLastname(event.target.value);
+  };
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (password === confirmPassword) {
+    //   console.log('Submitted:', username, password, email);
+      // Here you can add your signup logic, e.g. API calls to create a new user.
+      navigateTo('/loggin');
+
+    } else {
+      alert('Passwords do not match.');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Sign Up Form</h2>
+      <form onSubmit={handleSubmit}>
+      <div>
+          <label>Firstname:</label>
+          <input
+            type="text"
+            value={firstname}
+            onChange={handleFirstnameChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Lastname:</label>
+          <input
+            type="text"
+            value={lastname}
+            onChange={handleLastnameChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Confirm Password:</label>
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
 }
